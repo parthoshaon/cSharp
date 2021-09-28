f644ab1ba73e4d8e3d7886ea194c47379c30f1f7
Partho Shaon <parthoshaon@gmail.com>
2021-09-28 22:26:54 +0600

    Serial has not been maintained, everyone has to update and submit

---
+++ Person.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThirdGT
+{
+    class Yourname
+    {
+        private string name;
+        private string id;
+        private string program;
+        private int semester;
+        private double cgpa;
+
+        //property
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string Program
+        {
+            get; set;
+        }
+
+        public double Cgpa
+        {
+            get; set;
+        }
+
+        public int Semester
+        {
+            get; set;
+        }
+
+        //empty
+        public Yourname()
+        {
+            Console.WriteLine("Empty");
+        }
+        //param
+        public Yourname(string name, int semester, double cgpa, string id, string program)
+        {
+            Console.WriteLine("Param");
+            this.name = name;
+            this.id = id;
+            this.cgpa = cgpa;
+            this.semester = semester;
+            this.program = program;
+        }
+        //static
+        static Yourname()
+        {
+            Console.WriteLine("Static Constructor");
+        }
+
+        //copy
+        public Yourname(Yourname par)
+        {
+            name = par.name;
+            id = par.id;
+            semester = par.semester;
+            program = par.program;
+            cgpa = par.cgpa;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+        public void setSemester(int semester)
+        {
+            this.semester = semester;
+        }
+        public void setId(string id)
+        {
+            this.id = id;
+        }
+        public void setProgram(string program)
+        {
+            this.program = program;
+        }
+        public void setCgpa(double cgpa)
+        {
+            this.cgpa = cgpa;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public int getSemester()
+        {
+            return semester;
+        }
+
+        public double getCgpa()
+        {
+            return cgpa;
+        }
+
+        public string getProgram()
+        {
+            return program;
+        }
+
+        public string getId()
+        {
+            return id;
+        }
+
+
+        public void showDetails()
+        {
+            Console.WriteLine("Name: "+ name);
+            Console.WriteLine("Semester: "+ semester);
+            Console.WriteLine("ID: "+ id);
+            Console.WriteLine("Program: "+ program);
+            Console.WriteLine("CGPA: "+ cgpa);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ThirdGT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("**************");
+            Console.WriteLine("Param Constructor with Static Constructor");
+            Yourname p1 = new Yourname("Bruce Wayne", 10, 3.98, "15-29798-2", "CSSE");
+            p1.showDetails();
+            Console.WriteLine("**************");
+            Console.WriteLine("Empty Constructor");
+            Yourname p2 = new Yourname();
+            p2.setSemester(12);
+            p2.setName("Habib Rahman");
+            p2.setCgpa(4);
+            p2.setId("12-43234-5");
+            p2.setProgram("SE");
+            Console.WriteLine("Name: "+p1.getName());
+            Console.WriteLine("ID: "+p1.getId());
+            Console.WriteLine("Semester: "+p1.getSemester());
+            Console.WriteLine("CGPA: "+p1.getCgpa());
+            Console.WriteLine("Program: "+p1.getProgram());
+            Console.WriteLine("**************");
+            Console.WriteLine("Empty Constructor with Property");
+            Yourname p3 = new Yourname();
+
+            p3.Name = "Wade Wilson";
+            Console.WriteLine("Name: " + p3.Name);
+            p3.Id = "19-56473-7";
+            Console.WriteLine("ID: " + p3.Id);
+            p3.Cgpa = 2.9;
+            Console.WriteLine("CGPA: " + p3.Cgpa);
+            p3.Program = "CSE";
+            Console.WriteLine("Program: " + p3.Program);
+            p3.Semester = 48;
+            Console.WriteLine("Semester: " + p3.Semester);
+
+            Console.WriteLine();
+
+            Console.WriteLine("**************");
+            Console.WriteLine("Copy Constructor");
+            Yourname p4 = new Yourname(p1);
+            p4.showDetails();
+
+            Console.ReadKey();
+
+        }
+    }
+}
